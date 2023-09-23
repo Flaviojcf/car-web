3ffd86b0937c78ea86281ab33152771d630b94ef
Flavio <fjcf@poli.br>
2023-09-23 00:49:36 -0300

    refactor: add news properties to custom button component

--- src/app/components/CustomButton/index.tsx
+++ src/app/components/CustomButton/index.tsx
@@ -1,20 +1,35 @@
 'use client'
 
 import ICustomButton from '@/app/interfaces/ICustomButton'
+import Image from 'next/image'
 
 export function CustomButton({
   text,
   containerStyle,
   handleClick,
+  textStyles,
+  btnType,
+  isDisabled,
+  rightIcon,
 }: ICustomButton) {
   return (
     <button
       disabled={false}
-      type={'button'}
+      type={btnType || 'button'}
       className={`custom-btn ${containerStyle}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{text}</span>
+      <span className={`flex-1 ${textStyles}`}>{text}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="Right Icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   )
 }
