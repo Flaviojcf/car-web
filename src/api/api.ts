aa55bf7d08f76b754f2eb172fb6df5d07088fe93
Flavio <fjcf@poli.br>
2023-09-23 00:48:58 -0300

    feat: add type to function return

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,20 +1,22 @@
+import ICarCard from '@/app/interfaces/ICarCard'
 import axios from 'axios'
 
 const options = {
   method: 'GET',
   url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
-  params: { model: 'corolla' },
   headers: {
     'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
   },
 }
 
-export async function fetchCars() {
+export async function fetchCars(): Promise<ICarCard[]> {
   try {
     const response = (await axios.request(options)).data
-    return response
+
+    return response as ICarCard[]
   } catch (error) {
     console.error(error)
+    throw error
   }
 }
