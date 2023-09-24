6944c46805c6e4c98209752ee10a8bf162dd5bfe
Flavio <fjcf@poli.br>
2023-09-24 15:07:38 -0300

    refactor: set dynamic url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,20 +1,21 @@
 import ICarCard from '@/app/interfaces/ICarCard'
+import { IFilter } from '@/app/interfaces/IFilter'
 import axios from 'axios'
 
-const options = {
-  method: 'GET',
-  url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
-  headers: {
-    'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
-  },
-}
-
-export async function fetchCars(): Promise<ICarCard[]> {
+export async function fetchCars(filters: IFilter): Promise<ICarCard[]> {
+  const { fuel, limit, manufacturer, model, year } = filters
+  const url = `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`
   try {
-    const response = (await axios.request(options)).data
+    const response = await axios.request({
+      method: 'GET',
+      url,
+      headers: {
+        'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
+      },
+    })
 
-    return response as ICarCard[]
+    return response.data as ICarCard[]
   } catch (error) {
     console.error(error)
     throw error
