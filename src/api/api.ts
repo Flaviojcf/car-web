d35a97121a171037febbb33f68182971e73c86ed
Flavio <fjcf@poli.br>
2023-09-25 18:24:34 -0300

    add: break line

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,6 +4,7 @@ import axios from 'axios'
 
 export async function fetchCars(filters: IFilter): Promise<ICarCard[]> {
   const { fuel, limit, manufacturer, model, year } = filters
+
   const url = `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`
   try {
     const response = await axios.request({
